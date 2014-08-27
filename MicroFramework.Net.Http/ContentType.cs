76f987244179bfd61f763144ebda99fb8b36657a
aberteau <amael.berteau@tech-easy.fr>
2014-08-27 19:43:55 +0000

    Ajout HttpListenerResponseExtensions

---
+++ MicroFramework.Net.Http/ContentType.cs
@@ -0,0 +1,10 @@
+using System;
+using Microsoft.SPOT;
+
+namespace Techeasy.MicroFramework.Net.Http
+{
+    public class ContentType
+    {
+        public const string ApplicationJson = "application/json";
+    }
+}
---
+++ MicroFramework.Net.Http/HttpListenerResponseExtensions.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Net;
+using System.Text;
+using Microsoft.SPOT;
+using Json.NETMF;
+
+namespace Techeasy.MicroFramework.Net.Http
+{
+    public static class HttpListenerResponseExtensions
+    {
+        public static void WriteJson(this HttpListenerResponse response, object obj)
+        {
+            string json = JsonSerializer.SerializeObject(obj);
+            byte[] messageBody = Encoding.UTF8.GetBytes(json);
+            response.ContentType = ContentType.ApplicationJson;
+            response.ContentLength64 = messageBody.Length;
+            response.OutputStream.Write(messageBody, 0, messageBody.Length);
+            response.OutputStream.Close();
+        }
+    }
+}
